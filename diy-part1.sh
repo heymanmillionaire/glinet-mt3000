f3c0c0a4fce92765cfe0568e662b917c5065baf8
heymanmillionaire <heymanmillionaire@proton.me>
2025-07-09 22:04:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,7 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo "src-git fancontrol https://github.com/JiaY-shi/fancontrol.git" >> feeds.conf.default
 # echo 'src-git ImmortalWrt-luci https://github.com/openwrt/luci.git' >>feeds.conf.default
 # echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
